fae979f2e52891615d195e849e9b9dfa635449f7
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2022-12-04 22:05:49 +0700

    day01: Complete

--- src/mysolution/day01.py
+++ src/mysolution/day01.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
+import dataclasses
+import heapq
+from functools import cached_property
+from typing import TextIO
+
+import more_itertools
+from rich import print
+
 from mysolution.helpers.cli import command_with_input_file, open_input_file
 
 
@@ -8,15 +16,39 @@ from mysolution.helpers.cli import command_with_input_file, open_input_file
 def program(input_file):
     """Main program.
     """
-    data = read_input(input_file)
-    print(data)
+    with open_input_file(input_file) as fobj:
+        elves = read_input(fobj)
+
+    # Part 1: find the most calories carried by an elf
+    p1_calories = max(elf.total_calories for elf in elves)
+    print("Part 1:", p1_calories)
+
+    # Part 2: find the sum of calories carried by top-3 elves
+    top_three = heapq.nlargest(3, (elf.total_calories for elf in elves))
+    p2_calories = sum(top_three)
+    print("Part 2:", p2_calories)
 
 
-def read_input(input_file):
+@dataclasses.dataclass(frozen=True)
+class Elf:
+    """Information associated with a single elf.
+    """
+    #: List of calories carried by the elf
+    calories: list[int]
+
+    @cached_property
+    def total_calories(self) -> int:
+        return sum(self.calories)
+
+
+def read_input(fobj: TextIO) -> list[Elf]:
     """Reads and parses input file according to problem statement.
     """
-    with open_input_file(input_file) as fobj:
-        return [int(line) for line in fobj]
+    calories_groups = more_itertools.split_at(fobj, lambda line: not line.strip())
+    return [
+        Elf(calories=[int(calorie) for calorie in calories])
+        for calories in calories_groups
+    ]
 
 
 if __name__ == '__main__':
